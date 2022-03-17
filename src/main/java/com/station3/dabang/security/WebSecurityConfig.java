53560462a2a39599db0a8c2c8a6d981ec6c62573
ChoiSangIl <wo566@naver.com>
2022-03-17 19:44:02 +0900

    fix: web scurity ignore url swagger 추가

--- src/main/java/com/station3/dabang/security/WebSecurityConfig.java
+++ src/main/java/com/station3/dabang/security/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // Allow swagger to be accessed without authentication
     web.ignoring().antMatchers("/v3/api-docs")//
         .antMatchers("/swagger-resources/**")//
-        .antMatchers("/swagger-ui/index.html")//
+        .antMatchers("/swagger-ui/*")//
         .antMatchers("/configuration/**")//
         .antMatchers("/webjars/**")//
         .antMatchers("/public")
